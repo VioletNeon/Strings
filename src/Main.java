225a8e3ce397baaf6d2f3f7683e689a1d739a79d
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-11-06 21:42:32 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task-1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
     }
 }
